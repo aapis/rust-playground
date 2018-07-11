032f3ab7e8523d30a7fbf8998567ba63da5925d4
Ryan Priebe <rpriebe@me.com>
2018-07-11 17:35:31 -0600

    sample object with string and integer fields

--- objects.rs
+++ objects.rs
@@ -1,8 +1,25 @@
+use std::string::String;
+
 struct Person {
-    name: char,
+    name: String,
     age: i32
 }
 
 impl Person {
-    
+    fn get_name(&self) -> &String {
+        &self.name
+    }
+
+    fn get_age(&self) -> i32 {
+        self.age
+    }
+}
+
+fn main() {
+    let me = Person {
+        name: "Ryan".to_string(),
+        age: 29
+    };
+
+    println!("I am {} and I am {} years old", me.get_name(), me.get_age());
 }
